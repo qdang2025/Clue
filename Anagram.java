c50b4d768593fc2d65eb52c8063959dfb171eb8d
qdang2025 <93963410+qdang2025@users.noreply.github.com>
2022-11-19 17:43:21 -0500

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,5 +1,3 @@
-package com.gradescope.anagram; // DO NOT MODIFY PACKAGE NAME OR CLASS NAME
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -7,7 +5,6 @@ import java.util.Scanner;
 
 public class Anagram {
 
-	// The type of the dictionary variable can be of your choice.
 	private ArrayList<String> dictionary;
 	private Word[] words;
 
@@ -17,12 +14,7 @@ public class Anagram {
 		this.words = copyDictionary(dictionary);
 	}
 
-	// Read the dictionary file into some data structure of your choice. ArrayList
-	// is given as an example.
 	public ArrayList<String> loadDictionary(String filename) {
-		// You can use any other data structure as well, provided you build it from
-		// scratch
-		// or use some primitive data structure like list.
 		ArrayList<String> dictionary = new ArrayList<String>();
 		try {
 			// Read the file here.
@@ -39,11 +31,6 @@ public class Anagram {
 
 
 	/**
-	 * Implement the algorithm here. Do not change the function signature.
-	 * The return type should be ArrayList<ArrayList<String>>, so that Gradescope
-	 * can read your output.
-	 * Your output need not be sorted.
-	 * 
 	 * @returns - List of anagram classes, where each element in the list is a list
 	 *          of all the words in that anagram class.
 	 */
@@ -66,7 +53,7 @@ public class Anagram {
 	}
 
 	/**
-	 * You can use this method for testing and debugging if you wish.
+	 * Main method for testing and debugging
 	 */
 
 	public static void main(String[] args) {
